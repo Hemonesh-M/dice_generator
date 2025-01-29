9544fe67a07461d3d9344fa24ef28932b76f717e
HEMONESH MAHESHWARI <143785826+Hemonesh-M@users.noreply.github.com>
2025-01-29 16:26:52 +0530

    Update dice_roller.dart
    Made Icon clickable as well by wrapping it in  InkWell

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -15,15 +15,16 @@ class _DiceRollerState extends State<DiceRoller> {
   int diceValue = 1;
   void rollDice() {
     //0->5 + 1== 1->6
-    setState(
-            () 
-            { diceValue = randomizer.nextInt(6) + 1; }
-            );
+    setState(() {
+      diceValue = randomizer.nextInt(6) + 1;
+    });
   }
+
   void resetoOne() {
     //0->5 + 1== 1->6
-    setState(() { diceValue = 1; }
-            );
+    setState(() {
+      diceValue = 1;
+    });
   }
 
   @override
@@ -31,18 +32,20 @@ class _DiceRollerState extends State<DiceRoller> {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Image.asset('assests/dice-images/dice-$diceValue.png', width: 200),
-        // const SizedBox(
-        //   height: 200,
-        // ),
+        InkWell(
+          onTap: rollDice,
+          onLongPress: resetoOne,
+          child: Image.asset('assests/dice-images/dice-$diceValue.png',
+              width: 200),
+        ),
         TextButton(
-          onPressed: rollDice,
-          onLongPress:resetoOne ,
           style: TextButton.styleFrom(
             padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 100),
-            foregroundColor: const Color.fromARGB(255, 245, 245, 245),
+            foregroundColor: const Color.fromARGB(255, 255, 255, 255),
             textStyle: const TextStyle(fontSize: 60),
           ),
+          onPressed: rollDice,
+          onLongPress: resetoOne,
           child: const Text('Roll a Dice'),
         ),
       ],
